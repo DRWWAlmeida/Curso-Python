7e18c0d9216905af60b2c8394b68f2694843e493
Davi Almeida <davialmeidacont@gmail.com>
2023-11-09 22:00:56 -0300

    Projeto até aula 7.Adicionando Recipes na admin do Django

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 from recipes import views
 
@@ -9,3 +10,7 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
