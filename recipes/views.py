e7f0a6a08e719ef8fda06af73816632f6a6783a1
Davi Almeida <davialmeidacont@gmail.com>
2023-11-21 21:31:30 -0300

    Projeto até aula F055 18.Mostrando a página de detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404
+# from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 
@@ -13,20 +12,19 @@ def home(request):
 
 
 def category(request, category_id):
-   recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-
-   if not recipes:
-      raise Http404('Not found')
-
+   #recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+   recipes = get_list_or_404(
+      Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+   )
    return render(request, 'recipes/pages/category.html', context={
       'recipes': recipes,
-      'title': f'{recipes.first().category.name} - Category'
+      'title': f'{recipes[0].category.name} - Category'
    })
 
 
 def recipe(request, id):
-   recipes = Recipe.objects.filter(is_published=True)
+   recipe = Recipe.objects.filter(pk=id, is_published=True).first()
    return render(request, 'recipes/pages/recipe-view.html', context={
-      'recipe': recipes,
+      'recipe': recipe,
       'is_detail_page': True,
    })
