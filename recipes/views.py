c83bf0951d7670b704c5d645cf953a61c0158816
Davi Almeida <davialmeidacont@gmail.com>
2023-11-22 21:12:22 -0300

    Projeto até aula F060 24.Internacionalização, Time Zone e Tradução do Django para portugues - PT-BR

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 # from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -7,6 +7,7 @@ def home(request):
    recipes = Recipe.objects.filter(is_published=True)
    return render(request, 'recipes/pages/home.html', context={
       'recipes': recipes,
+      'title' : 'Home'
    })
 
 
@@ -23,8 +24,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-   recipe = Recipe.objects.filter(pk=id, is_published=True).first()
+   recipe = get_object_or_404(Recipe, pk=id, is_published=True)
    return render(request, 'recipes/pages/recipe-view.html', context={
+      #'title': f'{recipe.title}, | Recipe'
       'recipe': recipe,
       'is_detail_page': True,
    })
