fc41d200a573747f72e6d3342f29320354815ca5
Davi Almeida <davialmeidacont@gmail.com>
2023-11-24 21:18:42 -0300

    Projeto até aula F062 2.Iniciando com testes no Django com Pytest, Pytest-Django e Unittest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        variavel = 1234
+        assert 1 == 1, 'um não é igual a dois'
+        print('ola')
+
+
+
