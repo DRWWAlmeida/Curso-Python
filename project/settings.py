58f7f91744503d4dc607c7da9c4a472a609ba90d
Davi Almeida <davialmeidacont@gmail.com>
2023-11-28 21:56:33 -0300

    Projeto até aula F066 8.Separando os testes em um package chamado tests com init.py

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%e&ek%6(e_15l3gcwg3z%!%1y4ixj2#bb@i$_guh2h0dnofv6'
+SECRET_KEY = 'django-insecure-0%e&ek%6(e_15l3gcwg3z%!%1y4ixj2#bb@i$_guh2h0dnofv6'  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
